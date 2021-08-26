8bb2443a506a5c5bcf0ee2374e47a885eb864256
natalliasdh <natallia.sdh@gmail.com>
2021-08-26 11:40:10 -0400

    All dimensions without lightbox

--- js/app2.js
+++ js/app2.js
@@ -1,24 +1,70 @@
 let imageNumber = localStorage.getItem("imageid");
+var images2;
+
+connect();
 
 
-var xhr2 = new XMLHttpRequest;
-xhr2.onreadystatechange = function() {
-    if (xhr2.readyState === 4) {
+function connect() {
+    var xhr2 = new XMLHttpRequest;
+    xhr2.onreadystatechange = function() {
+        if (xhr2.readyState === 4) {
 
-        var images2 = JSON.parse(xhr2.responseText);
+            images2 = JSON.parse(xhr2.responseText);
 
-        console.log(images2[imageNumber].name);
-        document.getElementById("nameimage").innerHTML = images2[imageNumber].name;
-        document.getElementById("year").innerHTML = images2[imageNumber].year;
-        document.querySelector(".text").innerHTML = images2[imageNumber].description;
-        document.getElementById("nameauthor").innerHTML = images2[imageNumber].artist.name;
-        document.getElementById("authorphoto").innerHTML = `<img src="${images2[imageNumber].artist.image}">`;
-        document.querySelector(".largeimage").style.backgroundImage = `url("${images2[imageNumber].images.hero.small}")`;
+
+            showSlides(imageNumber);
+
+
+        }
+
+
+
+    };
+
+    xhr2.open('GET', 'data/data.json');
+    xhr2.send();
+}
+
+
+function showSlides(imageNumber) {
+
+
+
+
+
+    document.querySelectorAll(".nameimage")[0].innerHTML = images2[imageNumber].name;
+    document.querySelector(".source").innerHTML = `<a href="${images2[imageNumber].source}" targer="blank">GO TO SOURCE</a>`;
+    document.getElementById("year").innerHTML = images2[imageNumber].year;
+    let y = (parseInt(imageNumber) + 1) / parseInt(images2.length) * 100;
+    document.getElementById("progress").style.width = `${y}%`;
+
+    document.querySelector(".text").innerHTML = images2[imageNumber].description;
+    document.querySelectorAll(".nameauthor")[0].innerHTML = images2[imageNumber].artist.name;
+    document.querySelectorAll(".nameauthor")[1].innerHTML = images2[imageNumber].artist.name;
+    document.getElementById("authorphoto").innerHTML = `<img src="${images2[imageNumber].artist.image}">`;
+    document.querySelector(".largeimage").style.backgroundImage = `url("${images2[imageNumber].images.hero.small}")`;
+
+
+
+
+
+}
+
+
+function plusSlides(n) {
+
+    imageNumber = parseInt(imageNumber) + n;
+
+    if (imageNumber >= images2.length - 1) {
+        imageNumber = 0;
+    }
+    if (imageNumber < 0) {
+        imageNumber = images2.length - 1;
     }
 
 
+    console.log(imageNumber);
+    showSlides(imageNumber);
 
-};
 
-xhr2.open('GET', 'data/data.json');
-xhr2.send();
+}
